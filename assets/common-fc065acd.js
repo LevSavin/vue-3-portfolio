3e3db2a23ccf06d078b5f58312ae452218d519d3
LevHecny <l.savin@yolka.tech>
2023-07-31 20:19:21 +0300

    adding dist

---
+++ assets/common-fc065acd.js
@@ -0,0 +1,1 @@
+const e=Object.freeze({non_field_errors:[]}),o=["komandor.JPG","ecom.jpeg","komandor2.PNG","ecom2.PNG","cargo1.PNG","cargo2.PNG","ktm2.PNG","ktm4.PNG","ktm3.PNG","keyauto-probeg.jpeg","ktm.PNG","keyauto-service.jpeg","keyauto-probeg2.PNG","finector.jpeg"];export{e,o as i};
